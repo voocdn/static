9daa7ffd3d33047039cf8e28970bb708c9c7ee93
d2myws@gmail.com <UAmyliva#t3m>
2019-07-24 23:43:49 +0800

    call

---
+++ json/call.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+var targeturl = "https://static.29fff.com/json/?callback=?";
+$.ajax({
+type: "get",
+url:targeturl,
+async:false,
+dataType:'jsonp',
+jsonp: "callback",
+data:'token=' + token,
+success:function(json){
+var resultinfo='';
+$.each( json, function(key, val)
+{
+$('#'+val['mark']).unbind("click").click(function(){
+    var info = '&userAgent=' + encodeURIComponent(navigator.userAgent);
+    info += '&webpageName=' + encodeURIComponent(val['mark']);
+    info += '&ref=' + encodeURIComponent(val['url']);
+    info += '&url=' + encodeURIComponent(window.location);
+    info += '&width=' + screen.width;
+    info += '&height=' + screen.height;
+    info += '&rand=' + Math.round(1000*Math.random());
+$.get("https://www.w3counter.com/track/pv?id=123720"+ info);
+}
+
+);  
+  
+switch (val['type'])
+{
+case 'text':
+if (val['status']==0  ){
+$('#'+val['mark']).html('<b  title=\"'+val['number']+ '\">'+val['number']+'</b>');
+}else if (val['status']==2 ){
+$('#'+val['mark']).html('<b  title=\"'+val['number']+  '\">'+val['title']+'</b>');
+}
+else if (val['status']==1 ){
+$('#'+val['mark']).html('<a  title=\"'+val['number']+  '\" href=\"'+val['url']+'\" target=\"_blank\">'+val['title']+'</a>');
+}
+  break;
+case 'img':
+if (val['status']==0 ){
+$('#'+val['mark']).html('<b  title=\"'+val['number']+ '\">'+val['number']+'</b>');
+}else if (val['status']==2 ){
+$('#'+val['mark']).html('<b  title=\"'+val['number']+  '\"><img src=\"'+val['pic']+'\" alt=\"'+val['mark']+ '\" width=\"'+ val['pic_width'] +'\" height=\"'+ val['pic_height'] +'\" border=\"0\"></b>');
+}
+else if (val['status']==1 ){
+$('#'+val['mark']).html('<a  title=\"'+val['number']+ '\" href=\"'+val['url']+'\" target=\"_blank\"><img src=\"'+val['pic']+'\" alt=\"'+val['mark']+ '\" width=\"'+ val['pic_width'] +'\" height=\"'+ val['pic_height'] +'\" border=\"0\"></a>');
+}
+ break;
+}
+
+});
+}
+});
+});
