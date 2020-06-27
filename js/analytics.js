33bf5074ea6978f928b3995fa339432d3f49b43a
d2myws <d2myws@gmail.com>
2020-06-27 14:47:45 +0800

    commit message

---
+++ js/analytics.js
@@ -0,0 +1,11 @@
+(function(){
+var t1=document.createElement("script"),t0=document.getElementsByTagName("script")[0];
+t1.async=true;
+t1.src='https://www.googletagmanager.com/gtag/js?id='+uaid;
+t1.charset='UTF-8';
+t0.parentNode.insertBefore(t1,t0);
+})();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', uaid);
---
+++ json/call.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+var now = new Date();        
+var tstr = now.getFullYear() + '' + now.getMonth() + '' + now.getDate() + ''+ now.getHours();
+$.ajax({
+type: "get",
+url:targeturl,
+async:false,
+dataType:'jsonp',
+jsonp: "callback",
+jsonpCallback:"supercall"+tstr,
+cache:true,
+data:'token=' + token,
+success:function(json){
+var resultinfo='';
+$.each( json, function(key, val)
+{
+$('#'+val['mark']).unbind("click").click(function(){
+    var info = '&userAgent=' + encodeURIComponent(navigator.userAgent);
+    info += '&webpageName=' + encodeURIComponent(val['mark']);
+    info += '&ref=' + encodeURIComponent(val['url']);
+    info += '&url=' + encodeURIComponent(window.location);
+    info += '&width=' + screen.width;
+    info += '&height=' + screen.height;
+    info += '&rand=' + Math.round(1000*Math.random());
+$.get(trackurl + trackid + info);
+}
+
+);  
+  
+switch (val['type'])
+{
+case 'text':
+if (val['status']==0  ){
+$('#'+val['mark']).html('<b  title=\"'+val['number']+ '\">'+val['number']+'</b>');
+}else if (val['status']==2 ){
+$('#'+val['mark']).html('<b  title=\"'+val['number']+  '\">'+val['title']+'</b>');
+}
+else if (val['status']==1 ){
+$('#'+val['mark']).html('<a  title=\"'+val['number']+  '\" href=\"'+val['url']+'\" target=\"_blank\">'+val['title']+'</a>');
+}
+  break;
+case 'img':
+if (val['status']==0 ){
+$('#'+val['mark']).html('<b  title=\"'+val['number']+ '\">'+val['number']+'</b>');
+}else if (val['status']==2 ){
+$('#'+val['mark']).html('<b  title=\"'+val['number']+  '\"><img src=\"'+val['pic']+'\" alt=\"'+val['mark']+ '\" width=\"'+ val['pic_width'] +'\" height=\"'+ val['pic_height'] +'\" border=\"0\"></b>');
+}
+else if (val['status']==1 ){
+$('#'+val['mark']).html('<a  title=\"'+val['number']+ '\" href=\"'+val['url']+'\" target=\"_blank\"><img src=\"'+val['pic']+'\" alt=\"'+val['mark']+ '\" width=\"'+ val['pic_width'] +'\" height=\"'+ val['pic_height'] +'\" border=\"0\"></a>');
+}
+ break;
+}
+
+});
+}
+});
+});
